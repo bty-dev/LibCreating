443ae0267df13a86114eca9823bb5bd027cfa6f9
bty-dev <81759658+bty-dev@users.noreply.github.com>
2022-10-08 14:55:51 +0500

    Добавьте файлы проекта.

---
+++ CarActions/CarActions.cs
@@ -0,0 +1,26 @@
+﻿namespace CarActions
+{
+    public class Car
+    {
+        string modelName;
+        public Car(string modelName)
+        {
+            this.modelName = modelName;
+        }
+
+        public string getModelName()
+        {
+            return this.modelName;
+        }
+
+        public void openTheDoor()
+        {
+            Console.WriteLine("Door is open");
+        }
+
+        public void engineStart()
+        {
+            Console.WriteLine("Engine is started!");
+        }
+    }
+}
---
+++ LibCreating/Program.cs
@@ -0,0 +1,9 @@
+﻿using CarActions;
+
+Car car = new Car("Model s");
+
+var name = car.getModelName();
+Console.WriteLine(name);
+
+car.openTheDoor();
+car.engineStart();
